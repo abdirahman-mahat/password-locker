a4e455a1405e2723da7e620bde2f0dc93b4c3b0c
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-09-30 23:02:06 +0300

    all tests have been run sucessfully

--- run.py
+++ run.py
@@ -9,7 +9,26 @@ def create_user(p_name,u_name,ps):
     '''
     new_user = User(p_name,u_name,ps)
     return new_user
-
+def save_user(user):
+    '''
+    function to save user
+    '''
+    user.save_user()
+def del_user(user):
+    '''
+    function to delete a user
+    '''
+    user.del_user()
+def find_user(username):
+    '''
+    function that finds a user by platform_name and returns the user
+    '''
+    return User.find_by_username(username)
+def check_existing_user(username):
+    '''
+    function that check if a user exists with that platform_name and return a boolean
+    '''
+    return User.user_exists(username)
 def display_user():
     '''
     function that returns all the saved users
